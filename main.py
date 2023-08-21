90d759873adf36586d97cc5dc3f90d2b36e1238f
ghubisz <gab.op.pl@op.pl>
2023-08-22 01:00:18 +0200

    window pop

--- main.py
+++ main.py
@@ -8,3 +8,27 @@ pygame.init()
 
 pygame.display.set_caption("Platform game")
 
+BG_COLOR = (255, 255, 255)
+WIDTH, HEIGHT = 1000, 800
+FPS = 60
+PLAYER_VEL = 5
+
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+def main(window):
+    clock = pygame.time.Clock()
+
+    run = True
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+    pygame.quit()
+    quit()
+
+if __name__ == "__main__":
+    main(window)
